deebb35eb1632ab2d0b7d74812fdba8b4f030ea7
Egor Krais <krais1989@gmail.com>
2019-07-11 00:53:37 +0300

    hub connection/disconnect template

--- GoodMarket.ChatHubs/Hubs/MainChatHub.cs
+++ GoodMarket.ChatHubs/Hubs/MainChatHub.cs
@@ -22,6 +22,38 @@ namespace GoodMarket.ChatHubs
     /// </summary>
     public class MainChatHub : Hub<IClientChatHub>
     {
+        public async override Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+
+            var user = Context.User;
+            var connId = Context.ConnectionId;
+            var abortToken = Context.ConnectionAborted;
+
+            var stored = Context.Items;
+
+            await Groups.AddToGroupAsync(connId, "", abortToken);
+        }
+
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            var user = Context.User;
+            var connId = Context.ConnectionId;
+            var abortToken = Context.ConnectionAborted;
+
+            if (exception != null)  // ошибка сети и тп
+            {
+
+            }
+            else // connection.stop() на клиенте
+            {
+
+            }
+
+            await Groups.AddToGroupAsync(connId, "", abortToken);
+            await base.OnDisconnectedAsync(exception);
+        }
+
         public async Task SendMessageFromUser(string user, string message)
         {
             await Clients.All.SendMessageFromUser(user, message);
